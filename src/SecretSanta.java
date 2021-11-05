b750e9ed23af0015d3e9fcba554e16f56cb849b7
smit-celvix <smit@celvix.ca>
2021-11-04 22:25:47 -0600

    merged files into just one

--- src/SecretSanta.java
+++ src/SecretSanta.java
@@ -128,3 +128,65 @@ class PeopleAvoidanceComparator implements Comparator<Participant>
 	}
 
 }
+class Participant
+{
+	private  String name;
+	private  List<String> peopleToAvoid;
+	private String secretSantaFor;
+	private boolean isTaken;
+
+
+	public Participant(String name)
+	{
+		this.name = name;
+		this.peopleToAvoid = new ArrayList<>();
+		this.secretSantaFor = "";
+		this.isTaken = false;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public List<String> getPeopleToAvoid()
+	{
+		return peopleToAvoid;
+	}
+
+	public void setPeopleToAvoid(List<String> peopleToAvoid)
+	{
+		this.peopleToAvoid = peopleToAvoid;
+	}
+
+	public String getSecretSantaFor()
+	{
+		return secretSantaFor;
+	}
+
+	public void setSecretSantaFor(String secretSantaFor)
+	{
+		this.secretSantaFor = secretSantaFor;
+	}
+
+	public boolean isTaken()
+	{
+		return isTaken;
+	}
+
+	public void setTaken(boolean taken)
+	{
+		isTaken = taken;
+	}
+
+	@Override
+	public String toString()
+	{
+		return getName() + " is secret santa for " + getSecretSantaFor();
+	}
+}
